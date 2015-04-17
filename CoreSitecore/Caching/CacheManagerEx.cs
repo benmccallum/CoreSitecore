fb4e778be9a0085ddbff132c5153dfb1590dd0bd
Ben McCallum <ben.mccallum@isobar.com>
2015-04-17 15:50:53 +1000

    Added missing cache manager class.

---
+++ CoreSitecore/Caching/CacheManagerEx.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CoreSitecore.Caching
+{
+    /// <summary>
+    /// Helper methods around the Sitecore CacheManager.
+    /// </summary>
+    public static class CacheManagerEx
+    {
+        public static Sitecore.Caching.Cache GetSqlPrefetchCache(string databaseName)
+        {
+            return Sitecore.Caching.CacheManager.FindCacheByName("SqlDataProvider - Prefetch data(" + databaseName + ")");
+        }
+    }
+}
