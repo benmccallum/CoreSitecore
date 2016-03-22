fbeb6a8cf96d560dcd9d77f5fbdbe52629d88f6b
Ben McCallum <ben.mccallum@live.com.au>
2016-03-22 18:45:44 +1100

    Support appending a returnUrl to authorization redirects.

--- CoreSitecore/Sys/Web/Mvc/AuthorizeByDomainAttribute.cs
+++ CoreSitecore/Sys/Web/Mvc/AuthorizeByDomainAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sitecore.Data;
+using System;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,9 +17,31 @@ namespace CoreSitecore.Sys.Web.Mvc
         /// </summary>
         public string RedirectToUrl { get; set; }
 
+        /// <summary>
+        /// If specified, on an unauthorized request, user will be redirected to Sitecore Item with this ID.
+        /// RedirectToUrl takes precedence though if set.
+        /// </summary>
+        public Guid? RedirectToItemWithId { get; set; }
+
+        /// <summary>
+        /// If true, will append returnUrl querystring to url of redirected to page to support kickback later.
+        /// Default: true
+        /// </summary>
+        public bool AppendReturnUrlQueryString { get; set; }
+
+        /// <summary>
+        /// The querystring key/name to use if appending a return url.
+        /// Default: returnUrl
+        /// </summary>
+        public string ReturnUrlQueryStringKey { get; set; }
+
         public AuthorizeByDomainAttribute(string domainName)
         {
             _domainName = domainName;
+
+            // Defaults
+            AppendReturnUrlQueryString = true;
+            ReturnUrlQueryStringKey = "returnUrl";
         }
 
         protected override bool AuthorizeCore(HttpContextBase httpContext)
@@ -29,12 +52,35 @@ namespace CoreSitecore.Sys.Web.Mvc
 
         protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
         {
+            var redirectionUrl = string.Empty;
             if (!string.IsNullOrWhiteSpace(RedirectToUrl))
             {
-                filterContext.Result = new RedirectResult(RedirectToUrl);
+                redirectionUrl = RedirectToUrl;
+            }
+            else if (RedirectToItemWithId.HasValue)
+            {
+                var itemToRedirectTo = Sitecore.Context.Database.GetItem(new ID(RedirectToItemWithId.Value));
+                if (itemToRedirectTo != null)
+                {
+                    redirectionUrl = itemToRedirectTo.GetItemUrl();
+                }
             }
 
-            base.HandleUnauthorizedRequest(filterContext);
+            if (!string.IsNullOrWhiteSpace(redirectionUrl))
+            {
+                if (AppendReturnUrlQueryString)
+                {
+                    var currentUrl = filterContext.RequestContext.HttpContext.Request.Url.PathAndQuery;
+                    var currentUrlEncoded = HttpUtility.UrlEncode(currentUrl);
+                    redirectionUrl = CoreSystem.Helpers.UrlHelper.AppendQueryString(redirectionUrl, ReturnUrlQueryStringKey, currentUrlEncoded);
+                }
+
+                filterContext.Result = new RedirectResult(redirectionUrl);
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
         }
     }
 }
