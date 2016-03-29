d9c32ed85a60168aa3951942de55efe039ba1c89
Ben McCallum <ben.mccallum@live.com.au>
2016-03-29 17:40:07 +1100

    Release 8.1.9, added ability to compress jpeg images going through the Sitecore media handler with a querystring.

---
+++ CoreSitecore/Resources/Media/ChangeJpegCompressionLevelService.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Resources.Media;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace CoreSitecore.Resources.Media
+{
+    /// <summary>
+    /// Changes a jpeg image compression.
+    /// </summary>
+    /// <remarks>
+    /// Original source: https://laubplusco.net/make-sitecore-deliver-images-fits-screen/
+    /// </remarks>
+    public class ChangeJpegCompressionLevelService
+    {
+        public static Stream Change(MediaStream mediaStream, int jpegQuality)
+        {
+            var jpegEncoder = GetEncoder(ImageFormat.Jpeg);
+            var qualityEncoder = Encoder.Quality;
+            var encoderParameters = new EncoderParameters(1);
+            var bitmap = new Bitmap(mediaStream.Stream);
+            encoderParameters.Param[0] = new EncoderParameter(qualityEncoder, jpegQuality);
+            var stream = new MemoryStream();
+            bitmap.Save(stream, jpegEncoder, encoderParameters);
+            return stream;
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            var codecs = ImageCodecInfo.GetImageDecoders();
+            return codecs.FirstOrDefault(codec => codec.FormatID == format.Guid);
+        }
+    }
+}
---
+++ CoreSitecore/Resources/Media/SetJpegCompressionProcessor.cs
@@ -0,0 +1,58 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Resources.Media;
+using Sitecore.Web;
+using System;
+
+namespace CoreSitecore.Resources.Media
+{
+    /// <summary>
+    /// Set's Jpeg compression in the media handler pipeline if the correct querystring is supplied.
+    /// </summary>
+    /// <remarks>
+    /// Original source: https://laubplusco.net/make-sitecore-deliver-images-fits-screen/
+    /// </remarks>
+    public class SetJpegCompressionProcessor
+    {
+        public static string JpegCompressionLevelQueryKey
+        {
+            get { return Settings.GetSetting("CoreSitecore.Media.JpegCompressionLevelQueryKey", "jq"); }
+        }
+
+        public void Process(GetMediaStreamPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            if (args.Options.Thumbnail || !IsJpegImageRequest(args.MediaData.MimeType))
+            {
+                return;
+            }
+
+            if (args.OutputStream == null || !args.OutputStream.AllowMemoryLoading)
+            {
+                return;
+            }
+
+            var jpegQualityQuery = WebUtil.GetQueryString(JpegCompressionLevelQueryKey);
+            if (string.IsNullOrEmpty(jpegQualityQuery))
+            {
+                return;
+            }
+
+            int jpegQuality;
+            if (!int.TryParse(jpegQualityQuery, out jpegQuality) || jpegQuality <= 0 || jpegQuality > 100)
+            {
+                return;
+            }
+
+            var compressedStream = ChangeJpegCompressionLevelService.Change(args.OutputStream, jpegQuality);
+            args.OutputStream = new MediaStream(compressedStream, args.MediaData.Extension, args.OutputStream.MediaItem);
+        }
+
+        protected bool IsJpegImageRequest(string mimeType)
+        {
+            return mimeType.Equals("image/jpeg", StringComparison.InvariantCultureIgnoreCase)
+                || mimeType.Equals("image/pjpeg", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
