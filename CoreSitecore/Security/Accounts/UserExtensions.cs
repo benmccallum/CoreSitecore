cc658c7b3c72ad59f11d52ceed916bcba983d7f1
Ben McCallum <ben.mccallum@live.com.au>
2016-04-08 16:45:09 +1000

    Release 8.1.16, added extension methods for checking auth and domains off User.

---
+++ CoreSitecore/Security/Accounts/UserExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Security.Accounts;
+
+namespace CoreSitecore.Security.Accounts
+{
+    /// <summary>
+    /// Extension methods off of Sitecore.Security.Accounts.User.
+    /// </summary>
+    public static class UserExtensions
+    {
+        /// <summary>
+        /// Is the user authenticated and do they belong to the "extranet" domain.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static bool IsAuthenticatedInExtranet(this User user)
+        {
+            return user.IsAuthenticatedIn("extranet");
+        }
+
+        /// <summary>
+        /// Is the user authenticated and do they belong to the <paramref name="domain"/>.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        public static bool IsAuthenticatedIn(this User user, string domain)
+        {
+            return user.IsAuthenticated && user.GetDomainName() == domain;
+        }
+    }
+}
