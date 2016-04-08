40f210c082b1f438a5c9bbc74cc652fa30eefeda
Ben McCallum <ben.mccallum@live.com.au>
2016-04-08 17:07:34 +1000

    Release 8.1.17. Removing namespace around extension method.

--- CoreSitecore/Security/Accounts/UserExtensions.cs
+++ CoreSitecore/Security/Accounts/UserExtensions.cs
@@ -1,31 +1,28 @@
 ﻿using Sitecore.Security.Accounts;
 
-namespace CoreSitecore.Security.Accounts
+/// <summary>
+/// Extension methods off of Sitecore.Security.Accounts.User.
+/// </summary>
+public static class UserExtensions
 {
     /// <summary>
-    /// Extension methods off of Sitecore.Security.Accounts.User.
+    /// Is the user authenticated and do they belong to the "extranet" domain.
     /// </summary>
-    public static class UserExtensions
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public static bool IsAuthenticatedInExtranet(this User user)
     {
-        /// <summary>
-        /// Is the user authenticated and do they belong to the "extranet" domain.
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        public static bool IsAuthenticatedInExtranet(this User user)
-        {
-            return user.IsAuthenticatedIn("extranet");
-        }
+        return user.IsAuthenticatedIn("extranet");
+    }
 
-        /// <summary>
-        /// Is the user authenticated and do they belong to the <paramref name="domain"/>.
-        /// </summary>
-        /// <param name="user"></param>
-        /// <param name="domain"></param>
-        /// <returns></returns>
-        public static bool IsAuthenticatedIn(this User user, string domain)
-        {
-            return user.IsAuthenticated && user.GetDomainName() == domain;
-        }
+    /// <summary>
+    /// Is the user authenticated and do they belong to the <paramref name="domain"/>.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="domain"></param>
+    /// <returns></returns>
+    public static bool IsAuthenticatedIn(this User user, string domain)
+    {
+        return user.IsAuthenticated && user.GetDomainName() == domain;
     }
 }
